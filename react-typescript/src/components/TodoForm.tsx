9d34c9043abf5a07030d6ca95cbd6b7773634405
Iudin_Aleksei <aviordream@gmail.com>
2021-01-17 20:23:04 +0300

    feat: add todo handlers

--- react-typescript/src/components/TodoForm.tsx
+++ react-typescript/src/components/TodoForm.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 export const TodoForm: React.FC = () => {
+  // const [title, setTitle] = useState<string>('');
+  const ref = useRef<HTMLInputElement>(null);
+
+  // const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setTitle(event.target.value)
+  // }
+
+  const keyPressHandler = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      console.log(ref.current!.value)
+      ref.current!.value = ''
+      // console.log(title)
+      // setTitle('')
+    }
+  }
+
   return (
-    <div className="input-field">
-      <input type="text" id="title" />
+    <div className="input-field mt2">
+      <input
+        // onChange={changeHandler}
+        // value={title}
+        ref={ref}
+        type="text"
+        id="title"
+        placeholder="Введите название дела"
+        onKeyPress={keyPressHandler}
+      />
       <label htmlFor="title" className="active">
         Введите название дела
       </label>
