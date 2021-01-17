5ff5cebae3c9dad8648645cffb86438ae9e5184c
Iudin_Aleksei <aviordream@gmail.com>
2021-01-17 22:22:15 +0300

    feat: add todos handlers

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar } from './components/Navbar';
 import { TodoForm } from './components/TodoForm';
+import { TodoList } from './components/TodoList';
+import { ITodo } from './interfaces';
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState<ITodo[]>([])
+
+  const addHandler = (title: string) => {
+    const newTodo: ITodo = {
+      title: title,
+      id: Date.now(),
+      complited: false
+    }
+    setTodos(prev => [newTodo, ...prev])
+  }
+
+  const toggleHandler = (id: number) => {
+    setTodos(prev =>
+      prev.filter(todo => todo.id !== id)
+  )
+  }
+
+  const removeHandler = (id: number) => {
+    setTodos(prev =>
+      prev.filter(todo => todo.id !== id)
+  )
+
+  }
+
   return (<>
     <Navbar />
     <div className="container">
-      <TodoForm />
+      <TodoForm onAdd={addHandler} />
+      <TodoList
+        todos={todos}
+        onToggle={toggleHandler}
+        onRemove={removeHandler}
+      />
     </div>
   </>
   )
