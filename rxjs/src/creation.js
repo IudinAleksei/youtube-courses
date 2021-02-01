431ced548aeef8f7781f4f6dd1b561e1be599697
Aleksei Iudin <aleksei.iudin@stm-labs.ru>
2021-02-01 14:39:39 +0300

    feat: add fromEvent

--- rxjs/src/creation.js
+++ rxjs/src/creation.js
@@ -1,5 +1,5 @@
-import {of, from, Observable} from 'rxjs';
-import { scan } from 'rxjs/operators';
+import {of, from, Observable, fromEvent} from 'rxjs';
+import { scan, map } from 'rxjs/operators';
 
 // const stream$ = of(1, 2, 3, 4);
 
@@ -15,20 +15,51 @@ import { scan } from 'rxjs/operators';
 //   console.log('Value: ', val);
 // })
 
-const stream$ = new Observable(observer => {
-  observer.next('First value');
+// const stream$ = new Observable(observer => {
+//   observer.next('First value');
 
-  setTimeout(() => observer.next('after 1sec'), 1000)
+//   setTimeout(() => observer.next('after 1sec'), 1000)
 
-  setTimeout(() => observer.complete(), 1500)
+//   setTimeout(() => observer.complete(), 1500)
 
-  setTimeout(() => observer.error('error after 2sec'), 2000)
+//   setTimeout(() => observer.error('error after 2sec'), 2000)
 
-  setTimeout(() => observer.next('after 3sec'), 3000)
-})
+//   setTimeout(() => observer.next('after 3sec'), 3000)
+// })
 
-stream$.subscribe(
-  (v) => console.log('val: ', v),
-  (err) => console.log(err),
-  () => console.log('Complite')
+// stream$.subscribe(
+//   (v) => console.log('val: ', v),
+//   (err) => console.log(err),
+//   () => console.log('Complite')
+//   )
+
+// stream$.subscribe({
+//   next(val) {
+//     console.log('val: ', val)
+//   },
+//   error(err) {
+//     console.log(err)
+//   },
+//   complete() {
+//     console.log('Complite')
+//   }
+// })
+
+fromEvent(document.querySelector('canvas'), 'mousemove')
+  .pipe(
+    map(e => ({
+      x: e.offsetX,
+      y: e.offsetY,
+      ctx: e.target.getContext('2d')
+    }))
   )
+  .subscribe(pos => {
+    pos.ctx.fillRect(pos.x, pos.y, 2, 2)
+  })
+
+  const clear$ =fromEvent(document.getElementById('clear'), 'click')
+
+  clear$.subscribe(() => {
+    const canvas = document.querySelector('canvas')
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+  })
