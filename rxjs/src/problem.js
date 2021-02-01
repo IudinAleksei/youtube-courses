c6da77a0883e2294c4d1c979caaff8243ff7213b
Aleksei Iudin <aleksei.iudin@stm-labs.ru>
2021-02-01 14:05:23 +0300

    feat: add interval output

--- rxjs/src/problem.js
+++ rxjs/src/problem.js
@@ -1,3 +1,6 @@
+import { interval } from 'rxjs';
+import { filter, map, take } from 'rxjs/operators';
+
 const btn = document.getElementById('interval')
 const rxjsBtn = document.getElementById('rxjs')
 const display = document.querySelector('#problem .result')
@@ -11,3 +14,35 @@ const people = [
   {name: 'Irina', age: 23},
   {name: 'Oleg', age: 20}
 ]
+
+btn.addEventListener('click', () => {
+  btn.disabled = true;
+  let i = 0;
+  const canDrink = [];
+
+  const interval = setInterval(() => {
+    if (people[i]) {
+      if (people[i].age >= 18) {
+        canDrink.push(people[i].name); 
+      }
+      display.textContent = canDrink.join(' ');
+      i += 1;
+    } else {
+      clearInterval(interval);
+      btn.disabled = false;
+    }
+  }, 1000)
+})
+
+rxjsBtn.addEventListener('click', () => {
+
+  interval(1000)
+    .pipe(
+      take(people.length),
+      filter( v => people[v].age >= 18),
+      map(v => people[v].name)
+    )
+    .subscribe( res => {
+      display.textContent = res;
+    })
+})
