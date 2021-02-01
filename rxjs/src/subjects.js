8d4687657e5d94f77ecfd315ae86bb8f50a78654
Aleksei Iudin <aleksei.iudin@stm-labs.ru>
2021-02-01 15:20:56 +0300

    feat: add subjects

--- rxjs/src/subjects.js
+++ rxjs/src/subjects.js
@@ -1,12 +1,12 @@
-import {Subject} from 'rxjs';
+import {Subject, BehaviorSubject, ReplaySubject} from 'rxjs';
 
 document.addEventListener('click', () => {
 
-  const stream$ = new Subject(); 
-
-  stream$.subscribe((v) => console.log(v));
+  const stream$ = new ReplaySubject(2); 
 
   stream$.next('Hello');
   stream$.next('Rx');
   stream$.next('JS');
+
+  stream$.subscribe((v) => console.log(v));
 })
