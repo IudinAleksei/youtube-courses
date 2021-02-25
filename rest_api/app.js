cac6691f320db9c1b978b3cd4483a6fc0b94321b
Aleksei Iudin <aleksei.iudin@stm-labs.ru>
2021-02-25 16:53:42 +0300

    feat: add /contacts endpoint

--- rest_api/app.js
+++ rest_api/app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const CONTACTS = [
+  { id: 1, name: 'Vasya', value: '+7-900-000-00-00', marked: false },
+];
+
+app.get('/api/contacts', (req, res) => {
+  setTimeout(() => {
+    res.status(200).json(CONTACTS);
+  }, 100);
+});
+
 app.use(express.static(path.resolve(__dirname, 'client')));
 
 app.get('*', (req, res) => {
--- rest_api/client/frontend.js
+++ rest_api/client/frontend.js
@@ -1,13 +1,12 @@
 const VueApp = {
   data() {
     return {
+      loading: false,
       form: {
         name: '',
         value: '',
       },
-      contacts: [
-        { id: 1, name: 'Ivan', value: '+7-900-000-00-00', marked: false },
-      ],
+      contacts: [],
     };
   },
   computed: {
@@ -32,6 +31,45 @@ const VueApp = {
       this.contacts = this.contacts.filter((c) => c.id !== id);
     },
   },
+  async mounted() {
+    this.loading = true;
+    this.contacts = await request('/api/contacts');
+    this.loading = false;
+  },
 };
 
-Vue.createApp(VueApp).mount('#app');
+const app = Vue.createApp(VueApp);
+
+app.component('loader', {
+  template: `
+  <div style="display: flex; justify-content: center; align-items: center">
+    <div class="spinner-border text-primary" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+  `,
+});
+
+app.mount('#app');
+
+async function request(url, method = 'GET', data = null) {
+  try {
+    const headers = {};
+    let body;
+
+    if (data) {
+      headers['Content-Type'] = 'application/json';
+      body = JSON.stringify(data);
+    }
+
+    const response = await fetch(url, {
+      method,
+      headers,
+      body,
+    });
+
+    return await response.json();
+  } catch (e) {
+    console.warn('Error:', e.message);
+  }
+}
