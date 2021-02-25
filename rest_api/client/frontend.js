4ca7e15bb67ef6172e91b0eb7eeb03e61c123cdf
Aleksei Iudin <aleksei.iudin@stm-labs.ru>
2021-02-25 15:48:44 +0300

    feat: implement client

--- rest_api/client/frontend.js
+++ rest_api/client/frontend.js
@@ -5,11 +5,31 @@ const VueApp = {
         name: '',
         value: '',
       },
+      contacts: [
+        { id: 1, name: 'Ivan', value: '+7-900-000-00-00', marked: false },
+      ],
     };
+  },
+  computed: {
+    canCreate() {
+      return this.form.name.trim() && this.form.value.trim();
+    },
   },
   methods: {
     createContact() {
-      console.log(this.form);
+      const { ...contact } = this.form;
+      this.contacts.push({ ...contact, id: Date.now(), marked: false });
+
+      this.form.name = this.form.value = '';
+    },
+
+    markContact(id) {
+      const contact = this.contacts.find((c) => c.id === id);
+      contact.marked = true;
+    },
+
+    removeContact(id) {
+      this.contacts = this.contacts.filter((c) => c.id !== id);
     },
   },
 };
