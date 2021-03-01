5ee8e7b490b988d1547ffde96e94754f5f382dfc
Aleksei Iudin <aleksei.iudin@stm-labs.ru>
2021-03-01 15:20:09 +0300

    feat: add style-loaders and muicss

--- podcast-app/webpack.config.js
+++ podcast-app/webpack.config.js
@@ -19,4 +19,23 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
 };
