ffc9d8384a91a431c7c0657e448fb37203abf357
Aleksei Iudin <aleksei.iudin@stm-labs.ru>
2021-03-01 11:22:07 +0300

    feat: add createModal

--- modal/plugins/modal.js
+++ modal/plugins/modal.js
@@ -1,6 +1,29 @@
 function _createModal(options) {
-  const modal = document.createElement("div");
-  modal.classList.add("amodal");
+  const modal = document.createElement('div');
+  modal.classList.add('amodal');
+  modal.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <div class="modal-overlay">
+        <div class="modal-window">
+          <div class="modal-header">
+            <span class="modal-title">Modal title</span>
+            <span class="modal-close">&times;</span>
+          </div>
+          <div class="modal-body">
+            <p>Lorem ipsum dolor sit.</p>
+            <p>Lorem ipsum dolor sit.</p>
+          </div>
+          <div class="modal-footer">
+            <button>Ok</button>
+            <button>Cancel</button>
+          </div>
+        </div>
+      </div>  
+    `
+  );
+
+  return modal;
 }
 
 $.modal = function (options) {
