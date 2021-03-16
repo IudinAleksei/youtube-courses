3405f26257c4de973851a5fa00c25316505b0d53
Aleksei Iudin <aleksei.iudin@stm-labs.ru>
2021-03-16 17:26:56 +0300

    feat: add prototype, singelton, adapter and decorator

---
+++ js-patterns/1_creational/3_prototype.js
@@ -0,0 +1,15 @@
+const car = {
+  wheels: 4,
+
+  init() {
+    console.log('wheels', this.wheels, 'owner', this.owner);
+  },
+};
+
+const carWithOwner = Object.create(car, {
+  owner: {
+    value: 'Dima',
+  },
+});
+
+carWithOwner.init();
